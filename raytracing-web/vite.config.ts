cf869c01aed24ab662c2096eae619647fc4d2366
Andy Fiedler <andy@andyfiedler.com>
2022-07-01 17:51:31 -0400

    don't try loading https certs on CI

--- raytracing-web/vite.config.ts
+++ raytracing-web/vite.config.ts
@@ -2,6 +2,14 @@ import * as fs from "fs";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+let https = {};
+if (fs.existsSync("./.cert/key.pem") && fs.existsSync("./cert/cert.pem")) {
+  https = {
+    key: fs.readFileSync("./.cert/key.pem"),
+    cert: fs.readFileSync("./.cert/cert.pem"),
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -12,10 +20,7 @@ export default defineConfig({
     format: "es",
   },
   server: {
-    https: {
-      key: fs.readFileSync("./.cert/key.pem"),
-      cert: fs.readFileSync("./.cert/cert.pem"),
-    },
+    https,
     fs: {
       allow: [".."],
     },
