9bedf5a9bcb530612884a2f7e3847671d2161ed0
afiedler <afiedler@users.noreply.github.com>
2022-08-20 19:59:32 +0000

    deploy: 9da6634f519197b5dc0de186dea859906d69c75b

--- assets/worker.490e91d8.js
+++
@@ -1,1 +0,0 @@
-async function t(){let a=await import("./weblib-7b07c44b.js");await a.default(),a.run(),self.addEventListener("message",e=>{e.data.type==="raytrace"&&a.raytrace()}),self.postMessage({type:"ready"})}t();
